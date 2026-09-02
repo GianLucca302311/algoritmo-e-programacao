36d6b9536fa33cbae3e8cf5414e903fcdfc1c8db
Gian Lucca <159184514+GianLucca302311@users.noreply.github.com>
2026-09-02 11:15:06 -0300

    Add files via upload

---
+++ exemplo1.java
@@ -0,0 +1,6 @@
+public class exemplo1 {
+    public static void main(String[] args) {
+        int idade = 19;
+        System.out.printf("Eu tenho %d anos. %n", idade);
+    }
+}
---
+++ exemplo3.java
@@ -0,0 +1,7 @@
+public class exemplo3 {
+    public static void main(String[] args) {
+        String nome = "Gian";
+        int idade = 19;
+        System.out.printf("Nome: %-10s Idade : %d%n", nome, idade);
+    }
+}
